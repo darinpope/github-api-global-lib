2b53a9a4dc6a8af65bb7bbbc4478caa627e2c063
Darin Pope <darin@planetpope.com>
2018-04-26 17:26:28 -0400

    rough it in

---
+++ vars/deployApplication.groovy
@@ -0,0 +1,8 @@
+def call(Map config) {
+    echo 'inside deployApplication'
+    echo ${config.name}
+    sh 'env > env.txt'
+    for (String i : readFile('env.txt').split("\r?\n")) {
+        echo i
+    }
+}
---
+++ vars/publishArtifacts.groovy
@@ -0,0 +1,3 @@
+def call(Map config) {
+    echo 'inside publishArtifacts'
+}

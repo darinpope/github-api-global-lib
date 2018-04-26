b469a61ff9025e80479858f40af2c2f7b07a0572
Darin Pope <darin@planetpope.com>
2018-04-26 17:37:54 -0400

    add quotes

--- vars/deployApplication.groovy
+++ vars/deployApplication.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
     echo 'inside deployApplication'
-    echo ${config.name}
+    echo "${config.name}"
     sh 'env > env.txt'
     for (String i : readFile('env.txt').split("\r?\n")) {
         echo i

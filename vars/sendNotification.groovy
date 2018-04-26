007bf7ae55cc588906f8a01efba33791f73e4567
Darin Pope <darin@planetpope.com>
2018-04-26 19:52:49 -0400

    token replacement

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,5 +1,6 @@
 def call(Map config) {
     echo 'inside sendNotification'
     def rawBody = libraryResource 'com/planetpope/emailtemplate/build-results.html'
+    rawBody.replace("#APPLICATION_NAME#",env.JOB_BASE_NAME).replace("#SOURCE_BRANCH#",env.BRANCH_NAME).replace("#BUILD_NUMBER#",env.BUILD_NUMBER).replace("#DEVELOPER#",gitAuthorName()).replace("#BUILD_URL#",env.BUILD_URL)
     echo rawBody
 }

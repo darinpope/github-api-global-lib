6772be9f18ff78537c2f6970f328b383c2f79271
Darin Pope <darin@planetpope.com>
2018-04-26 20:05:16 -0400

    change variable

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
     echo 'inside sendNotification'
     def rawBody = libraryResource 'com/planetpope/emailtemplate/build-results.html'
     rawBody.replaceAll("#APPLICATION_NAME#",env.JOB_BASE_NAME)
-    rawBody.replaceAll("#SOURCE_BRANCH#",env.BRANCH_NAME)
+    rawBody.replaceAll("#SOURCE_BRANCH#",env.GIT_BRANCH)
     rawBody.replaceAll("#BUILD_NUMBER#",env.BUILD_NUMBER)
     rawBody.replaceAll("#DEVELOPER#",gitAuthorName())
     rawBody.replaceAll("#BUILD_URL#",env.BUILD_URL)

8c62792397bc115d73ddeab3ffb3ee52881874fb
Darin Pope <darin@planetpope.com>
2018-04-26 20:00:10 -0400

    change replacement

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,6 +1,10 @@
 def call(Map config) {
     echo 'inside sendNotification'
     def rawBody = libraryResource 'com/planetpope/emailtemplate/build-results.html'
-    rawBody.replace("#APPLICATION_NAME#",env.JOB_BASE_NAME).replace("#SOURCE_BRANCH#",env.BRANCH_NAME).replace("#BUILD_NUMBER#",env.BUILD_NUMBER).replace("#DEVELOPER#",gitAuthorName()).replace("#BUILD_URL#",env.BUILD_URL)
+    rawBody.replaceAll("#APPLICATION_NAME#",env.JOB_BASE_NAME)
+    rawBody.replaceAll("#SOURCE_BRANCH#",env.BRANCH_NAME)
+    rawBody.replaceAll("#BUILD_NUMBER#",env.BUILD_NUMBER)
+    rawBody.replaceAll("#DEVELOPER#",gitAuthorName())
+    rawBody.replaceAll("#BUILD_URL#",env.BUILD_URL)
     echo rawBody
 }

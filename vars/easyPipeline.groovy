08db821425a3e9ddf19f904ef525e706f14e18de
Darin Pope <darin@planetpope.com>
2018-05-01 17:10:25 -0400

    make it easy

---
+++ vars/easyPipeline.groovy
@@ -0,0 +1,52 @@
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent pipelineParams.agentLabel
+        stages {
+            stage("Prepare Build Environment") {
+                steps {
+                    prepareBuildEnvironment()
+                    helloWorld(name: "prepareBuildEnvironment")
+                    helloWorldExternal()
+                }
+            }
+            stage("Source Code Checkout") {
+                steps {
+                    echo 'scc'
+                }
+            }
+            stage("SonarQube Scan") {
+                when {
+                    branch 'master'
+                }
+                steps {
+                    echo 'scan'
+                }
+            }
+            stage("Build Application") {
+                steps {
+                    echo 'build'
+                }
+            }
+            stage("Publish Artifacts") {
+                steps {
+                    publishArtifacts(name: "publishArtifacts")
+                }
+            }
+            stage("Deploy Application") {
+                steps {
+                    deployApplication(name: "deployApplication")
+                }
+            }
+        }
+        post {
+            always {
+                sendNotification()
+            }
+        }
+    }
+}

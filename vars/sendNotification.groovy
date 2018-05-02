8d8ef4795dedbb39aabbb5a6d2ff1a79d365470a
Darin Pope <darin@planetpope.com>
2018-05-02 14:03:30 -0400

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,6 +1,6 @@
-@NonCPS
+//@NonCPS
 def renderTemplate(input, binding) {
-    def engine = new groovy.text.SimpleTemplateEngine()
+    def engine = new groovy.text.GStringTemplateEngine()
     def template = engine.createTemplate(input).make(binding)
     return template.toString()
 }

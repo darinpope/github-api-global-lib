e26168ff58d7e3bf9e19c3d20406b009f558dcab
Darin Pope <darin@planetpope.com>
2018-05-14 10:11:52 -0400

    Update and rename loadBashScript.groovy to loadLinuxScript.groovy

--- vars/loadBashScript.groovy
+++
@@ -1,4 +0,0 @@
-def call(Map config) { 
-  def scriptcontents = libraryResource "com/planetpope/bash/${config.name}"               
-  writeFile file: "${config.name}", text: scriptcontents 
-} 
---
+++ vars/loadLinuxScript.groovy
@@ -0,0 +1,4 @@
+def call(Map config) { 
+  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"               
+  writeFile file: "${config.name}", text: scriptcontents 
+} 

7555da92648b40c32f62ef32f6ed203fbea8a71e
Darin Pope <darin@planetpope.com>
2018-05-14 10:15:43 -0400

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config) {
-    def text = sh(returnStdout: true, script: './build-support-scripts/linux/hello-world.sh').trim()
-    echo text
+    loadLinuxScript('hello-world.sh')
+    bat './hello-world.sh'
 }

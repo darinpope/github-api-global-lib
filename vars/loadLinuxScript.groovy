48e3bd86778703b46814e8e7a658ba68801a1131
Darin Pope <darin@planetpope.com>
2018-05-22 14:11:07 -0400

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,6 +1,6 @@
 def call(Map config) { 
   def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"    
-  echo $scriptcontents
+  echo "$scriptcontents"
   writeFile file: "${config.name}", text: scriptcontents 
   echo "finished write"
 } 

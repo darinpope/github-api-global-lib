c6a9e1febc32ba1b0800f6c81dc8fc3b6ea123d0
Darin Pope <darin@planetpope.com>
2018-09-11 14:04:43 -0400

    Update closurePipeline.groovy

--- vars/closurePipeline.groovy
+++ vars/closurePipeline.groovy
@@ -1,2 +1,47 @@
 def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent { label "master" }
+        stages {
+            stage("echo parameters") {
+                steps {
+                    sh "env | sort"
+                }
+            }
+            stage("Prepare Build Environment") {
+                steps {
+                  echo 'prepare'
+                }
+            }
+            stage("Source Code Checkout") {
+                steps {
+                    echo 'scc'
+                }
+            }
+            stage("SonarQube Scan") {
+                steps {
+                    echo 'scan'
+                }
+            }
+            stage("Build Application") {
+                steps {
+                    echo 'build'
+                }
+            }
+            stage("Publish Artifacts") {
+                steps {
+                    echo 'publish'
+                }
+            }
+            stage("Deploy Application") {
+                steps {
+                    echo 'deploy'
+                }
+            }
+        }
+    }
 }

71853b6adca02cd896b2659c8d44299a2caadbde
Darin Pope <darin@planetpope.com>
2018-10-01 23:26:49 -0400

    Update addSidebarLink.groovy

--- vars/addSidebarLink.groovy
+++ vars/addSidebarLink.groovy
@@ -4,6 +4,6 @@ def call(Map config) {
     displayName: "testdarin",
     urlName: "http://www.darinpope.com",
     iconFileName: "foo"
-  ] as hudson.model.Action
+  ] as hudson.model.ProminentProjectAction
   currentBuild.rawBuild.addAction(action)
 }

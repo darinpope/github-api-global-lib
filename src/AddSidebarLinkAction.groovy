806e23c3a2d42f7ca9ea88b83b189c210fe782ff
Darin Pope <darin@planetpope.com>
2018-10-01 22:24:41 -0400

    Create AddSidebarLinkAction.groovy

---
+++ src/AddSidebarLinkAction.groovy
@@ -0,0 +1,24 @@
+import 
+class AddSidebarLinkAction implements hudson.model.Action {
+  private String displayName;
+  private String iconFileName;
+  private String urlName;
+  
+  public AddSidebarLinkAction(String displayName,String iconFileName,String urlName) {
+    this.displayName = displayName;
+    this.iconFileName = iconFileName;
+    this.urlName = urlName;
+  }
+  
+  public String getDisplayName() {
+    return displayName;
+  }
+  
+  public String getIconFileName() {
+    return iconFileName;
+  }
+  
+  public String getUrlName() {
+    return urlName;
+  }
+}

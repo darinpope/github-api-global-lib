96082b649ec6579c73055f1d15e98d9c15384a4b
Darin Pope <darin@planetpope.com>
2018-10-01 22:54:11 -0400

    Update AddSidebarLinkAction.groovy

--- src/AddSidebarLinkAction.groovy
+++ src/AddSidebarLinkAction.groovy
@@ -1,4 +1,4 @@
-class AddSidebarLinkAction implements hudson.model.Action,java.io.Serializable {
+public class AddSidebarLinkAction implements hudson.model.Action,java.io.Serializable {
   private String displayName;
   private String iconFileName;
   private String urlName;

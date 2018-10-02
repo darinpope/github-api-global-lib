76557df2585ed8c7c9a63f99585fe1c6ed100eac
Darin Pope <darin@planetpope.com>
2018-10-01 22:59:23 -0400

    Update addSidebarLink.groovy

--- vars/addSidebarLink.groovy
+++ vars/addSidebarLink.groovy
@@ -1,4 +1,4 @@
 def call(Map config) {
-  def action = new AddSidebarLinkAction("testLinkFromDarin",null,"http://www.darinpope.com/")
+  def action = new AddSidebarLinkAction("testLinkFromDarin",null,"http://www.darinpope.com/",null)
   currentBuild.rawBuild.getActions().add(action)
 }

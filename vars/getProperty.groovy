f15463a0665aec23fdc96816d76506ce026821c6
Darin Pope <darin@planetpope.com>
2018-10-02 21:57:03 -0400

    don’t use readProperties

--- vars/getProperty.groovy
+++ vars/getProperty.groovy
@@ -1,4 +1,9 @@
 def call(Map config) {
-    def props = readProperties(file: "${config.file}")
+    //def props = readProperties(file: "${config.file}")
+    Properties props = new Properties()
+    File propertiesFile = new File("${config.file}")
+    propertiesFile.withInputStream {
+        props.load(it)
+    }
     return props["${config.key}"]
 }

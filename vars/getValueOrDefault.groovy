4f27efcd50b5299baa0170fcc6ad8cd3b9ec2ddb
Darin Pope <darin@planetpope.com>
2018-12-05 22:07:24 -0500

    trying some new stuff

---
+++ vars/getValueOrDefault.groovy
@@ -0,0 +1,6 @@
+def call(String actualValue,String defaultValue) {
+    if (!actualValue?.trim()) {
+        return defaultValue?.trim()
+    }
+    return actualValue?.trim()
+}
---
+++ vars/indigenous.groovy
@@ -0,0 +1,132 @@
+def call(body) {
+
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    def autoDeploy = getValueOrDefault("${pipelineParams.autoDeploy}","n")
+    def runSmokeTests = getValueOrDefault("${pipelineParams.runSmokeTests}","n")
+    def notifyEmail = "${pipelineParams.notifyEmail}"
+
+    pipeline {
+        agent none
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '5'))
+        }
+        stages {
+            stage("Prepare Build Environment") {
+                steps {
+                    sh """
+                        echo My branch is: ${BRANCH_NAME}
+                        echo My build is: ${BUILD_NUMBER}
+                    """
+                }
+            }
+            stage("Build & Unit Tests") {
+                steps {
+                    sh "echo build and unit tests"
+                }
+            }
+
+            stage('SonarQube analysis') {
+                when {
+                    branch 'master'
+                }
+                steps {
+                    sh "echo SonarQube analysis"
+                }
+            }
+            stage("Upload to Binary Repository") {
+                steps {
+                    sh "echo Upload to Binary Repository"
+                }
+            }
+
+            stage("Xray Scan") {
+                steps {
+                    sh "echo Xray Scan"
+                }
+            }
+
+            stage("Deploy") {
+                when {
+                    equals expected: 'y', actual: ${autoDeploy}
+                }
+                parallel {
+                    stage("Deploy to Pre-Production") {
+                        when {
+                            branch 'master'
+                        }
+                        steps {
+                            checkpoint "deployToPreProduction"
+                            sh "echo Deploy to Pre-Production"
+                        }
+                    }
+
+                    stage("Deploy to Dev") {
+                        when {
+                            not { branch 'master' }
+                        }
+                        steps {
+                            checkpoint "deployToDev"
+                            sh "echo Deploy to Dev"
+                        }
+                    }
+                }
+            }
+
+            stage("Smoketests") {
+                when {
+                    equals expected: 'y', actual: ${runSmokeTests}
+                }
+                parallel {
+                    stage("GUI") {
+                        stages {
+                            stage("Checkpoint before GUI") {
+                                steps {
+                                    checkpoint "Run GUI Smoketest"
+                                }
+                            }
+                            stage("Run GUI Smoketest") {
+                                steps {
+                                    sh "echo Run GUI Smoketest"
+                                }
+                                post {
+                                    failure {
+                                        sendEmail(${notifyEmail})
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    stage("Service") {
+                        stages {
+                            stage("Checkpoint before Service") {
+                                steps {
+                                    checkpoint "Run Service Smoketest"
+                                }
+                            }
+                            stage("Run Service Smoketest") {
+                                steps {
+                                    sh "echo Run Service Smoketest"
+                                }
+                                post {
+                                    failure {
+                                        sendIndigenousEmail(to: "${notifyEmail}")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        post {
+            success {
+                sendIndigenousEmail(to: "${notifyEmail}")
+            }
+        }
+    }
+}

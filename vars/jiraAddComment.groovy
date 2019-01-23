b7df2472245d7d98bb1dcb14b2ff0af9e16bc7c3
Darin Pope <darin@planetpope.com>
2019-01-23 15:39:35 -0500

    Update jiraAddComment.groovy

--- vars/jiraAddComment.groovy
+++ vars/jiraAddComment.groovy
@@ -1,5 +1,15 @@
+def renderTemplate(input, binding) {
+    def engine = new groovy.text.GStringTemplateEngine()
+    def template = engine.createTemplate(input).make(binding)
+    return template.toString()
+}
+
 def call(Map config=[:]) {
-  def body = libraryResource "com/planetpope/api/jira/addComment.json"
+  def rawBody = libraryResource 'com/planetpope/api/jira/addComment.json'
+  def binding = [
+    applicationName: ${config.body}
+  ]
+  def render = renderTemplate(rawBody,binding)
   def jiraServer = "https://jiraserver"
   sh """
     curl -D- -u $JIRA_CREDENTIALS -X POST --data ${body} -H "Content-Type: application/json" $jiraServer/rest/api/2/issue/${config.issueId}/comment

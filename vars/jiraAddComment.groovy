17cbff9c9c1b5d9044c748e6c93b003ab7817a93
Darin Pope <darin@planetpope.com>
2019-01-23 16:26:37 -0500

    Update jiraAddComment.groovy

--- vars/jiraAddComment.groovy
+++ vars/jiraAddComment.groovy
@@ -12,6 +12,6 @@ def call(Map config=[:]) {
   def render = renderTemplate(rawBody,binding)
   def jiraServer = "https://jiraserver"
   sh """
-    curl -D- -u $JIRA_CREDENTIALS -X POST --data ${render} -H "Content-Type: application/json" $jiraServer/rest/api/2/issue/${config.issueId}/comment
+    curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $jiraServer/rest/api/2/issue/${config.issueId}/comment
   """
 }

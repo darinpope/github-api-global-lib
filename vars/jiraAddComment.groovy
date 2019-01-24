3e589da60f0e386cc847038082dbdc106837c47c
Darin Pope <darin@planetpope.com>
2019-01-24 18:31:37 -0500

    Update jiraAddComment.groovy

--- vars/jiraAddComment.groovy
+++ vars/jiraAddComment.groovy
@@ -5,6 +5,10 @@ def renderTemplate(input, binding) {
 }
 
 def call(Map config=[:]) {
+  def issueId = getIssueIdFromCommitMessage()
+  if( issueId?.trim() ) {
+    return
+  }
   def rawBody = libraryResource 'com/planetpope/api/jira/addComment.json'
   def binding = [
     body: "${config.body}"
@@ -12,6 +16,6 @@ def call(Map config=[:]) {
   def render = renderTemplate(rawBody,binding)
   def jiraServer = "https://jiraserver"
   sh """
-    curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $jiraServer/rest/api/2/issue/${config.issueId}/comment
+    curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $jiraServer/rest/api/2/issue/${issueId}/comment
   """
 }

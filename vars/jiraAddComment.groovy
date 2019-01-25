34b3840428d3cbb22e1496eee1a3d2a32cb53bc0
Darin Pope <darin@planetpope.com>
2019-01-24 19:23:09 -0500

    Update jiraAddComment.groovy

--- vars/jiraAddComment.groovy
+++ vars/jiraAddComment.groovy
@@ -7,7 +7,7 @@ def renderTemplate(input, binding) {
 def call(Map config=[:]) {
   def issueId = getIssueIdFromCommitMessage()
   echo "issueId = ${issueId}"
-  if( issueId?.trim() ) {
+  if( !issueId?.trim() ) {
     echo "bailing out"
     return
   }

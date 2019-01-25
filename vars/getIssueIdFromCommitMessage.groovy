d638b0af9e009a29e9386a0224541d0e41de627b
Darin Pope <darin@planetpope.com>
2019-01-25 08:45:52 -0500

    Update getIssueIdFromCommitMessage.groovy

--- vars/getIssueIdFromCommitMessage.groovy
+++ vars/getIssueIdFromCommitMessage.groovy
@@ -1,5 +1,9 @@
 def call(Map config=[:]) {
   def rawIssueId = sh(returnStdout: true,script: "git log --oneline --format=%B -n 1 $GIT_COMMIT | head -n 1 | cut -d' ' -f1")
   //echo "rawIssueId = ${rawIssueId}"
-  return rawIssueId.replaceAll("[^a-zA-Z0-9-]+","")
+  def issueId = rawIssueId.replaceAll("[^a-zA-Z0-9-]+","")
+  if( issueId ==~ /((?<!([A-Za-z]{1,10})-?)[A-Z]+-\d+)/ ) {
+    return issueId
+  }
+  return
 }

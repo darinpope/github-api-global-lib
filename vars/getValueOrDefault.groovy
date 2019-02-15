5f64ec4ecf1bd8acded22eae8b95762f9f174023
Darin Pope <darin@planetpope.com>
2019-02-15 09:20:23 -0500

    Update getValueOrDefault.groovy

--- vars/getValueOrDefault.groovy
+++ vars/getValueOrDefault.groovy
@@ -2,7 +2,7 @@ def call(String actualValue,String defaultValue) {
     echo "actualValue = " + actualValue
     echo "defaultValue = " + defaultValue
     
-    echo !actualValue?.trim().toString()
+    echo String.valueOf(!actualValue?.trim())
     
     if (!actualValue?.trim()) {
         return defaultValue?.trim()

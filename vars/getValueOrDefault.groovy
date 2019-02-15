e3bc033dbc703a2a1ea8191e09e97d023d58c0d4
Darin Pope <darin@planetpope.com>
2019-02-15 09:41:49 -0500

    Update getValueOrDefault.groovy

--- vars/getValueOrDefault.groovy
+++ vars/getValueOrDefault.groovy
@@ -1,6 +1,7 @@
 def call(String actualValue,String defaultValue) {  
     boolean containsData = (actualValue?.trim())
     if (containsData) {
+        echo "containsData = ***" + actualValue?.trim() + "***"
         return actualValue?.trim()
     }
     return defaultValue?.trim()

8a29df22a3bddc0bad259832fd79f7bf6758165c
Darin Pope <darin@planetpope.com>
2019-02-15 09:11:17 -0500

    Create wip.groovy

---
+++ vars/wip.groovy
@@ -0,0 +1,15 @@
+def call(body) {
+
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    def autoDeploy = getValueOrDefault("${pipelineParams.autoDeploy}","n")
+    def runSmokeTests = getValueOrDefault("${pipelineParams.runSmokeTests}","n")
+    def notifyEmail = "${pipelineParams.notifyEmail}"
+
+    echo $autoDeploy
+    echo $runSmokeTests
+    echo $notifyEmail
+}

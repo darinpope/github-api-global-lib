8db0daf0d341c738403556fb66300d1f6589c14e
Darin Pope <darin@planetpope.com>
2019-02-28 10:33:43 -0500

    Update deployContentProd.groovy

--- vars/deployContentProd.groovy
+++ vars/deployContentProd.groovy
@@ -2,7 +2,8 @@ def call(Map config=[:]) {
   def application
   def version
   def event = currentBuild?.getBuildCauses()[0]?.event?.event?.toString()
-  if(event == "deploy-content-prod") {
+  def causeClass = currentBuild?.getBuildCauses()[0]?._class
+  if(event == "deploy-content-prod" && causeClass == "com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause") {
     application = currentBuild?.getBuildCauses()[0]?.event?.application?.toString()
     version = currentBuild?.getBuildCauses()[0]?.event?.version?.toString()
   } else {

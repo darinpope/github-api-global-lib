419baedd7aa6f0aaec5e21f598b747475735387a
Darin Pope <darin@planetpope.com>
2019-07-12 23:30:22 -0400

    Update getPullRequestInfoInternal.groovy

--- vars/getPullRequestInfoInternal.groovy
+++ vars/getPullRequestInfoInternal.groovy
@@ -1,4 +1,5 @@
 def call(Map config=[:]) {
   String url = "https://api.github.com/repos/${config.owner}/${config.repo}/pulls/${config.pullNumber}"
+  //return sh(returnStdout: true, script: """curl -H "Authorization: token ${OAUTH-TOKEN}" $url""")
   return sh(returnStdout: true, script: """curl $url""")
 }

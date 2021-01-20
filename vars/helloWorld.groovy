9d08108c4e8e8436f4be4c5c8df2a889e07a20cb
Darin Pope <darin@planetpope.com>
2021-01-20 14:34:12 -0500

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,3 @@
 def call(Map config=[:]) {
-    // you can call any valid step functions from your code, just like you can from Pipeline scripts
-    sh 'echo Hello world, ${config.name}'
+    sh "echo Hello world, ${config.name}"
 }

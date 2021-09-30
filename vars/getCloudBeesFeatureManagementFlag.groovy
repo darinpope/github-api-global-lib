545239085e522e9c829c68d9c49f5227c607e8b4
Darin Pope <darin@planetpope.com>
2021-09-30 15:20:04 -0400

    add missing space
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/getCloudBeesFeatureManagementFlag.groovy
+++ vars/getCloudBeesFeatureManagementFlag.groovy
@@ -1,5 +1,5 @@
 def call(Map config=[:]) {
-  def curl = 'curl -H "Content-Type: application/json" -H "Authorization: Bearer' + env.CBFM_USER_TOKEN + '" https://x-api.rollout.io/public-api/applications/'+config.applicationId+'/'+config.environment+'/flags/'+config.flagName+"| jq 'if .enabled == true then .value else empty end'"
+  def curl = 'curl -H "Content-Type: application/json" -H "Authorization: Bearer ' + env.CBFM_USER_TOKEN + '" https://x-api.rollout.io/public-api/applications/'+config.applicationId+'/'+config.environment+'/flags/'+config.flagName+"| jq 'if .enabled == true then .value else empty end'"
   
   def flagValue = sh(returnStdout:true,script:curl).trim()
   return flagValue;

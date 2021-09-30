8d7549b41f508a9c12311a73ffd4965814984c9c
Darin Pope <darin@planetpope.com>
2021-09-30 15:09:39 -0400

    switch to curl
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/getCloudBeesFeatureManagementFlag.groovy
+++ vars/getCloudBeesFeatureManagementFlag.groovy
@@ -1,6 +1,6 @@
 def call(Map config=[:]) {
-  def flagValue = sh(returnStdout:true,script:"""
-  https --print 'b' --pretty none GET x-api.rollout.io/public-api/applications/${config.applicationId}/${config.environment}/flags/${config.flagName} 'authorization: Bearer ${env.CBFM_USER_TOKEN}'  | jq 'if .enabled == true then .value else empty end'
-  """).trim()
+  def flagValue = sh(returnStdout:true,script:'''
+  curl -H "Content-Type: application/json" -H "Authorization: Bearer ${env.CBFM_USER_TOKEN}" https://x-api.rollout.io/public-api/applications/${config.applicationId}/${config.environment}/flags/${config.flagName}  | jq 'if .enabled == true then .value else empty end'
+  ''').trim()
   return flagValue;
 }

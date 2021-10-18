e995eef9becff42fa2318bc896a4ed300440528e
Darin Pope <darin@planetpope.com>
2021-10-18 16:00:21 -0400

    refactor
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/addSidebarLink.groovy
+++ vars/addSidebarLink.groovy
@@ -1,9 +1,5 @@
-def call(Map config) {
-  def action = new hudson.plugins.sidebar_link.LinkAction("http://www.darinpope.com/","testLinkFromDarin","star.gif")
-  //action = [
-  //  displayName: "testdarin",
-  //  urlName: "http://www.darinpope.com",
-  //  iconFileName: "star.gif"
-  //] as hudson.model.PermalinkProjectAction
+def call(Map config=[:]) {
+  //def action = new hudson.plugins.sidebar_link.LinkAction("http://www.darinpope.com/","testLinkFromDarin","star.gif")
+  def action = new hudson.plugins.sidebar_link.LinkAction(config.url,config.text,config.icon)
   currentBuild.rawBuild.addAction(action)
 }

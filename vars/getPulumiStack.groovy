3d1a40929d9445318f19d669ac7b2db70639ffbb
Darin Pope <darin@planetpope.com>
2021-10-25 16:03:55 -0400

    remove echo
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/getPulumiStack.groovy
+++ vars/getPulumiStack.groovy
@@ -1,9 +1,6 @@
 def call() {
   def url = env.GIT_URL.split('/')
-  echo "url = " + url
   def branch = env.GIT_BRANCH.split('/')
-  echo "branch = " + branch
   def repoName = url[4].split('.git')
-  echo "repoName = " + repoName
   return url[3] + '/' + repoName[0] + '/' + branch[1]
 }

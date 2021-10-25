8c8bd45335325364fba144cae9d71d4a9ac73728
Darin Pope <darin@planetpope.com>
2021-10-25 14:00:07 -0400

    echoes
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/getPulumiStack.groovy
+++ vars/getPulumiStack.groovy
@@ -1,6 +1,9 @@
 def call() {
   def url = env.GIT_URL.split('/')
+  echo "url = " + url
   def branch = env.GIT_BRANCH.split('/')
+  echo "branch = " + branch
   def repoName = url[4].split('.')
+  echo "repoName = " + repoName
   return url[3] + '/' + repoName[0] + '/' + branch[1]
 }

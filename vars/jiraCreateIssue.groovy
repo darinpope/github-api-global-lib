752a775d200c06acd082156669e2c10088755fda
Darin Pope <darin@planetpope.com>
2021-10-29 17:01:23 -0400

    fix issues
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,13 +1,13 @@
 def call(Map config=[:]) {
   def rawBody = libraryResource 'com/planetpope/api/jira/createIssue.json'
   def binding = [
-    key: "${config.body}",
+    key: "${config.key}",
     summary: "${config.summary}",
     description: "${config.description}",
     issueTypeName: "${config.issueTypeName}"
   ]
   def render = renderTemplate(rawBody,binding)
-  sh """
+  sh '''
     curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $JIRA_URL/rest/api/2/issue
-  """
+  '''
 }

18f3f0bf1036e77a65e4516f6a707e097c0d18a2
Darin Pope <darin@planetpope.com>
2021-10-29 17:09:07 -0400

    build it
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -7,7 +7,5 @@ def call(Map config=[:]) {
     issueTypeName: "${config.issueTypeName}"
   ]
   def render = renderTemplate(rawBody,binding)
-  sh '''
-    curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $JIRA_URL/rest/api/2/issue
-  '''
+  sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
 }

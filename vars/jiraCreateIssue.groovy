888b4303bac6c173c375c8d89c0b1d109dffed0a
Darin Pope <darin@planetpope.com>
2021-10-29 16:59:11 -0400

    add jiraCreateIssue
    Signed-off-by: Darin Pope <darin@planetpope.com>

---
+++ vars/jiraCreateIssue.groovy
@@ -0,0 +1,13 @@
+def call(Map config=[:]) {
+  def rawBody = libraryResource 'com/planetpope/api/jira/createIssue.json'
+  def binding = [
+    key: "${config.body}",
+    summary: "${config.summary}",
+    description: "${config.description}",
+    issueTypeName: "${config.issueTypeName}"
+  ]
+  def render = renderTemplate(rawBody,binding)
+  sh """
+    curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $JIRA_URL/rest/api/2/issue
+  """
+}

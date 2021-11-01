043827e2212220b1989e90d0cd27bb9e6985b361
Darin Pope <darin@planetpope.com>
2021-11-01 16:39:41 -0400

    try render
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/jiraProcessChangeLogItems.groovy
+++ vars/jiraProcessChangeLogItems.groovy
@@ -6,5 +6,21 @@ def call(Map config=[:]) {
   }
   changeLogItems.each {
     println it.fromString + '; ' + it.toString
+    def fromTo = [it.fromString,it.toString]
+    switch(fromTo) {
+      case {it == ["Backlog","Selected for Development"]}
+        break;
+      case {it == ["Selected for Development","In Progress"]}
+        def component = currentBuild.getBuildCauses()[0]?.event?.issue?.fields?.components[0]?.name
+        def fixVersion = currentBuild.getBuildCauses()[0]?.event?.issue?.fields?.fixVersions[0]?.name
+        def m = [
+          event: "deploy-to-qa",
+          component: component,
+          version: fixVersion
+        ]
+        println m.inspect()
+        //publishEvent(jsonEvent('{"foo":"bar"}'))
+        break;
+    }
   }
 }

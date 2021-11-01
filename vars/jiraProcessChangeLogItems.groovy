caf10494d78896824855c0523b3123cb7a42f46d
Darin Pope <darin@planetpope.com>
2021-11-01 16:41:54 -0400

    add qa
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- vars/jiraProcessChangeLogItems.groovy
+++ vars/jiraProcessChangeLogItems.groovy
@@ -9,6 +9,15 @@ def call(Map config=[:]) {
     def fromTo = [it.fromString,it.toString]
     switch(fromTo) {
       case {it == ["Backlog","Selected for Development"]}
+        def component = currentBuild.getBuildCauses()[0]?.event?.issue?.fields?.components[0]?.name
+        def fixVersion = currentBuild.getBuildCauses()[0]?.event?.issue?.fields?.fixVersions[0]?.name
+        def m = [
+          event: "deploy-to-qa",
+          component: component,
+          version: fixVersion
+        ]
+        println m.inspect()
+        //publishEvent(jsonEvent('{"foo":"bar"}'))
         break;
       case {it == ["Selected for Development","In Progress"]}
         def component = currentBuild.getBuildCauses()[0]?.event?.issue?.fields?.components[0]?.name

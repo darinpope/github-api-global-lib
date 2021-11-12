1574643c383f5841e57a10369ce0924117c7e5a5
Darin Pope <darin@planetpope.com>
2021-11-12 17:15:18 -0500

    try this
    Signed-off-by: Darin Pope <darin@planetpope.com>

---
+++ vars/monoRepoPipeline.groovy
@@ -0,0 +1,69 @@
+def call(body) {
+
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
+  pipeline {
+    agent {
+      label "${pipelineParams.baseAgent}"
+    }
+    stages {
+      stage('Build Frontend') {
+        agent { 
+          docker { 
+            image "${pipelineParams.frontendAgent}"
+          }
+        }
+        when {
+          changeset "**/frontend/*.*"
+          beforeAgent true
+        }
+        steps {
+          dir('frontend') {
+            sh '''
+              npm --version
+              node --version
+              cat frontend.txt
+            '''
+          }
+        }
+      }
+      stage('Build Web') {
+        agent { 
+          docker { 
+            image "${pipelineParams.backendWebAgent}"
+          }
+        }
+        when {
+          changeset "**/backend/web/*.*"
+          beforeAgent true
+        }
+        steps {
+          dir ('backend/web') {
+            sh 'mvn --version'
+            sh 'cat web.txt'
+          }
+        }
+      }
+      stage('Build REST API') {
+        agent { 
+          docker { 
+            image "${pipelineParams.backendApiAgent}"
+          }
+        }
+        when {
+          changeset "**/backend/api/*.*"
+          beforeAgent true
+        }
+        steps {
+          dir ('backend/api') {
+            sh 'go version'
+            sh 'cat api.txt'
+          }
+        }
+      }
+    }
+  }
+}

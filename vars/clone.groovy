a5867aa354533c1d3987ad7e0f47377f9b86c7c0
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-03 11:07:00 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,1 +1,10 @@
-
+def call(String gitUrl, String branch) {
+    def gitCommand = """git clone ${gitUrl} -b ${branch}"""
+    
+    try {
+        sh(script: gitCommand, returnStatus: true)
+        echo "Git clone succeeded."
+    } catch (Exception e) {
+        error "Git clone failed: ${e.message}"
+    }
+}

115310470abae7ec2aee4103728efee7699dab11
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-03 11:44:47 +0530

    Create mySharedLibrary.groovy

---
+++ src/mySharedLibrary.groovy
@@ -0,0 +1,12 @@
+// mySharedLibrary.groovy
+def checkoutGitRepository(String repositoryUrl, String branch, String targetDir) {
+    node {
+        stage('Checkout') {
+            checkout([$class: 'GitSCM',
+                branches: [[name: branch]],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: targetDir]],
+                userRemoteConfigs: [[url: repositoryUrl]]])
+        }
+    }
+}

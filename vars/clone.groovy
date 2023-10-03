af0cd34022e605f424c9bd6c800dca49e6ef3ad0
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-03 17:53:02 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,20 +1,14 @@
 // mySharedLibrary.groovy
-def checkoutGitRepository(String repositoryUrl, String branch) {
+def checkoutGitRepository(String repositoryUrl, String branch, String targetDir) {
     node {
         stage('Checkout') {
             
            // Checkout the Git repository using the 'checkout' step
    checkout([$class: 'GitSCM',
-              branches: [[name: branch]],
-              userRemoteConfigs: [[url: repositoryUrl]]])
-              def checkoutDir = "${WORKSPACE}/my-repo-directory"
-              def targetDir = "${WORKSPACE}/my-target-directory"
-             sh "mkdir -p ${targetDir}"
-             // Create the target directory if it doesn't exist
-             sh "mkdir -p ${targetDir}"
-
-              // Move the checked-out code to the target directory
-                  sh "mv ${checkoutDir}/* ${targetDir}/"
+                branches: [[name: branch]],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: targetDir]],
+                userRemoteConfigs: [[url: repositoryUrl]]])
             echo "success"
         }
     }

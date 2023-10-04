379873b7e1a83917ba7fca942cf86263bbd00055
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-04 10:56:14 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -11,26 +11,13 @@ def checkoutGitRepository(String repositoryUrl, String branch, String targetDir)
                 userRemoteConfigs: [[url: repositoryUrl]]])
             echo "success"
         }
-        stage('Install maven'){
-           
-
- 
-     // Define the desired Maven version
-        def mavenVersion = '3.8.4'
-
-        // Define the URL for downloading Maven
-        def mavenUrl = "https://downloads.apache.org/maven/maven-3/${mavenVersion}/binaries/apache-maven-${mavenVersion}-bin.zip"
-
-        // Define the installation directory
-        def mavenHome = tool name: 'maven', type: 'ToolInstallation'
-
-     
-        }
+       
          stage('Build') {
-               def projectDir = pwd()
+             //  def projectDir = pwd()
         // Add commands or steps to build your code
-        sh "cd ${targetDir}"
-        sh "mvn clean install"  // Example: Building Java code with Maven
+        //sh "cd ${targetDir}"
+             eho "build step"
+       // sh "mvn clean install"  // Example: Building Java code with Maven
     }
     }
 }

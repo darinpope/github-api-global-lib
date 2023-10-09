8faa670cc8819208dfb7c001cdde1417dc23d6e0
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-09 17:56:32 +0530

    Update pass_parm.groovy

--- vars/pass_parm.groovy
+++ vars/pass_parm.groovy
@@ -1,4 +1,4 @@
-def call(String name, int empId, bool status) {
+def call(String name, int empId, boolean status) {
     // Your code here
  echo "Name: $name, Employee ID: $empId", IsActive: $status
 

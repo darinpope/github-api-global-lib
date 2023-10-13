eb7e2eb21f064f9d1a04129f9598c22415327235
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-13 12:04:57 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -19,7 +19,7 @@ def checkoutGitRepository(String repositoryUrl, String branch, String targetDir)
     }
           stage('readfile') {
       
-             def filePath = libraryResource "${env.WORKSPACE}/vars/files/test.txt"
+             def filePath = libraryResource "${env.WORKSPACE}/files/test.txt"
 
                     // Use the 'readFile' step to read the file content and store it in a variable
                     def fileContent = readFile(filePath)

3a72f61f0cd05b0bace70796771d9881664940e8
jibinamathew <83585188+jibinamathew@users.noreply.github.com>
2023-10-13 11:53:17 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -17,5 +17,16 @@ def checkoutGitRepository(String repositoryUrl, String branch, String targetDir)
              echo "build step.............."
        
     }
+          stage('readfile') {
+        
+             def filePath = 'files/test.txt'
+
+                    // Use the 'readFile' step to read the file content and store it in a variable
+                    def fileContent = readFile(filePath)
+
+                    // Now 'fileContent' contains the content of the file
+                    echo "File content: ${fileContent}"
+       
+    }
     }
 }
